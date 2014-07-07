d962bfa7f3d24c8a1e4d5a4df55192aef1312ab4
Keith Bowes <zooplah@gmail.com>
2014-07-06 22:36:15 -0400

    Added a German translation

---
+++ locales/de_DE/_global.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Global lang file
+ * This file was generated automatically from messages.po
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+$trans['de_DE'] = array(
+'__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
+'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2014-06-15 05:01-0400\nPO-Revision-Date: 2014-06-15 05:05-0400\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: German\nLanguage: de\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'Bad Behaviour Plugin for b2evolution' => 'Bad-Behaviour-Plugin für b2evolution',
+'The Web\'s premier link spam killer.' => 'Die beste Link-Spam-Mörder auf Web.',
+"\n<!-- %1\$s %2\$s, run time: %3\$s milliseconds -->\n" => "\n<!-- %1\$s %2\$s, Laufzeit: %3\$s Millisekunden -->\n",
+'Bad Behaviour' => 'Bad Behaviour',
+'Display Stats' => 'Anzeige die Statistik',
+'Strict' => 'Streng',
+'Strict checking (blocks more spam but may block some people)' => 'Strenge Prüfung (blockiert mehr Spam aber vielleicht Menschen auch)',
+'Logging' => 'Protokollierung',
+'HTTP request logging (recommended)' => 'Protokollierung der Anforderungen (empfohlen)',
+'Verbose Logging' => 'Wortreiche Protokollierung',
+'Log all requests' => 'Protokollieren alle Anforderung',
+'http:BL Access Key' => 'http:BL-Zugriffsschlüssel',
+'Minimum Threat Level (25 is recommended)' => 'Mindestbedrohungsstufe (25 ist empfohlen)',
+'Maximum Age of Data (30 is recommended)' => 'Maximales Alter der Daten (30 ist empfohlen)',
+'Offsite forms' => 'Offsite-Formulare',
+'Allow forms submitted from other websites' => 'Erlauben Formulare geschickt von anderen Websites',
+'Strict EU cookies' => 'Strenge EU-Cookies',
+'Disables cookie-based filters' => 'Desaktiviert Cookie-basierte Filter',
+'Reverse Proxy' => 'Reverse Proxy',
+'This site is behind a reverse proxy' => 'Diese Website ist hinter einem Reverse Proxy',
+'Reverse proxy header' => 'Nachrichtenkopf des Reverse Proxy',
+'Reverse proxy addresses' => 'Adressen des Reverse Proxy',
+'List of IP addresses of your reverse proxy.  ' => 'Liste IP-Adressen des ihren Reverse Proxy.  ',
+'One per line.' => 'Eine pro Zeile.',
+'Whitelist IP addresses' => 'Whitelist der IP-Adressen',
+'List of IP addresses that are never filtered.  ' => 'Liste IP-Adressen, die nie gefiltert werden.  ',
+'Whitelist user agents' => 'Whitelist der User Agents',
+'List of user agents that are never filtered.  ' => 'Liste User Agents, die nie gefiltert werden.  ',
+'Whitelist URLs' => 'Whitelist der URLs',
+'List of URLs that are never filtered.  ' => 'Liste URLS, die nie gefiltert werden.  ',
+'<h2>%s has blocked the following access attempts in the last 7 days.</h2>' => '<h2>%s hat die folgende Zugriffsversuche in die letzen 7 Tagen blockiert.</h2>',
+'IP' => 'IP',
+'More information about this ip address' => 'Mehr Informationen über diese IP-Adresse',
+'Date' => 'Datum',
+'Request URI' => 'Angeforderte URI',
+'View this uri on your blog' => 'Ansehen diese URI auf ihrem Blog',
+'HTTP Headers' => 'HTTP-Nachrichtenköpfe',
+'User Agent' => 'User Agent',
+'Explanation' => 'Beschreibung',
+'Code' => 'Code',
+'<p>A total of %d access attepts blocked.</p>' => '<p>Insgesamt %d Zugriffsversuche blockiert.</p>',
+'<p>More about <a href="http://www.bad-behavior.ioerror.us/">%s</a></p>' => '<p>Mehr über <a href="http://www.bad-behavior.ioerror.us/">%s</a></p>',
+"<div><a href=\"http://www.bad-behavior.ioerror.us/\"><cite>%1\$s</cite></a> has blocked %2\$d access attempts in the last 7 days.</div>\n" => "<div><a href=\"http://www.bad-behavior.ioerror.us/\"><cite>%1\$s</cite></a> hat %2\$d Zugriffsversuche in den letzen 7 Tage blockiert.</div>\n",
+
+);
+?>
