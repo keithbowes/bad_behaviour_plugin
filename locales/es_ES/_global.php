31c64b78b663b90eeda7a5846fcbb04c00b6e6db
Keith Bowes <zooplah@gmail.com>
2014-07-06 22:39:33 -0400

    Spanish translation

---
+++ locales/es_ES/_global.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ * Global lang file
+ * This file was generated automatically from messages.po
+ */
+if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
+
+$trans['es_ES'] = array(
+'__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
+'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: http://fplanque.net/\nPOT-Creation-Date: 2014-06-13 22:12-0400\nPO-Revision-Date: 2014-06-14 03:39-0400\nLast-Translator: Keith <keith@localhost>\nLanguage-Team: Spanish\nLanguage: es\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'Bad Behaviour Plugin for b2evolution' => 'Complemento de Bad Behaviour para b2evolution',
+'The Web\'s premier link spam killer.' => 'El mejor matador de spam de enlaces en el web',
+'Bad Behaviour' => 'Bad Behaviour',
+'milliseconds' => 'milisegundos',
+'run time: ' => 'tiempo de ejecución: ',
+'Display Stats' => 'Mostrar estadísticas',
+'Strict' => 'Estricto',
+'Strict checking (blocks more spam but may block some people)' => 'Comprobando estricto (bloquea más de spam pero quizá algunas personas también)',
+'Logging' => 'Registrando',
+'HTTP request logging (recommended)' => 'Registrando de peticiones HTTP (recomendado)',
+'Verbose Logging' => 'Registrando verboso',
+'Log all requests' => 'Registrar todos los peticiones',
+'http:BL Access Key' => 'Llave de acceso de http:BL',
+'Minimum Threat Level (25 is recommended)' => 'Nivel minimal de amenaza (25 es recomendado)',
+'Maximum Age of Data (30 is recommended)' => 'Edad maximal de la data (30 es recomendado)',
+'Offsite forms' => 'Formularios fuera del sitio',
+'Allow forms submitted from other websites' => 'Permitir formularios que estuvieron mandados de otros sitios web',
+'Strict EU cookies' => 'Cookies estrictas de la UE',
+'Disables cookie-based filters' => 'Desactiva filtros basados en cookies',
+'Reverse Proxy' => 'Proxy reverso',
+'This site is behind a reverse proxy' => 'Eso sitio está detrás de un proxy reverso',
+'Reverse proxy header' => 'Encabezado del proxy reverso',
+'Reverse proxy addresses' => 'Direcciones del proxy reverso',
+'List of IP addresses of your reverse proxy.  ' => 'Lista de las direcciones IP de su proxy reverso.  ',
+'One per line.' => 'Uno por línea.',
+'Whitelist IP addresses' => 'Lista blanca de direcciones IP',
+'List of IP addresses that are never filtered.  ' => 'Lista de direcciones IP que no son nunca filtrados.  ',
+'Whitelist user agents' => 'Lista blanca de agentes de usuario',
+'List of user agents that are never filtered.  ' => 'Lista de agentes de usuarios que no son nuca filtrados.  ',
+'Whitelist URLs' => 'Lista blanca de URLs',
+'List of URLs that are never filtered.  ' => 'Lista de URLS que no son nuca filtrados.  ',
+' has blocked the following access attempts in the last 7 days' => ' ha bloqueado los siguientes intentos de acceso en los últimos 7 días',
+'IP' => 'IP',
+'More information about this ip address' => 'Más de información sobre esta dirección IP',
+'Date' => 'Fecha',
+'Request URI' => 'URI pedido',
+'View this uri on your blog' => 'Ver esto URI en su blog',
+'HTTP Headers' => 'Encabezados HTTP',
+'User Agent' => 'Agente de usuario',
+'Explanation' => 'Explicación',
+'Code' => 'Código',
+' access attempts blocked.' => ' intentos de acceso bloqueados.',
+'A total of ' => 'Un total de ',
+'More about ' => 'Más sobre ',
+'has blocked {accessattempts} access attempts in the last 7 days.' => 'ha bloqueado {accessattempts} intentos de acceso en los últimos 7 días.',
+"\n<!-- %s result was %s! This request would have been blocked. -->\n" => "\n<!-- El resultado de %s fue %s! Esta petición habría bloqueado. -->\n",
+
+);
+?>
